478bdc7ad28b451e7e85661a413754d65ff466fe
SakibAbdullah <65773431+SakibAbdullah@users.noreply.github.com>
2020-12-09 00:03:13 +0600

    updated

---
+++ final/finaltask3/model/homeModel.php
@@ -0,0 +1,77 @@
+<?php
+        function viewData($id){
+            $servername = "localhost";
+            $username = "root";
+            $pass = "";
+            $dbname = "user";
+            
+            $conn = new mysqli($servername, $username, $pass, $dbname);
+           
+            if ($conn->connect_error) {
+             die("Connection failed: " . $conn->connect_error);
+            }
+        
+        
+            $sql = "SELECT * FROM userinfo WHERE id='".$id."'";
+            $result = $conn->query($sql);
+        
+            if ($result->num_rows > 0) {
+            
+                $row = $result->fetch_assoc();
+                return $row;
+                $conn->close();
+            } 
+            else {
+                echo "0 results";
+                $conn->close();
+            }
+        }
+
+        function deleteId($id){
+            $servername = "localhost";
+            $username = "root";
+            $pass = "";
+            $dbname = "userdata";
+            
+            
+            $conn = new mysqli($servername, $username, $pass, $dbname);
+            
+            if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+            }
+            
+            
+            $sql = "DELETE FROM userinfo WHERE id='".$id."'";
+            
+            if ($conn->query($sql) === TRUE) {
+              echo "Record deleted successfully";
+            } else {
+              echo "Error deleting record: " . $conn->error;
+            }
+            
+            $conn->close();
+        }
+
+        function updateEmail($id,$email){
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "userdata";
+        
+            
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            
+            if ($conn->connect_error) {
+             die("Connection failed: " . $conn->connect_error);
+            }
+        
+            $sql = "UPDATE userinfo SET email='".$email."' WHERE id='".$id."'";
+        
+            if ($conn->query($sql) === TRUE) {
+            echo "Record updated successfully";
+            } else {
+            echo "Error updating record: " . $conn->error;
+            }
+        
+            $conn->close();
+        }
