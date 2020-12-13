0cadfdc11d5e71aa468e61b2aaa1b0849aa6a302
SakibAbdullah <65773431+SakibAbdullah@users.noreply.github.com>
2020-12-13 10:38:00 +0600

    updated

---
+++ final/finaltask4/controller/loginController.php
@@ -0,0 +1,17 @@
+  
+<?php
+    require('../model/loginModel.php');
+    $id = $password = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $id = $_POST["id"];
+        $password = $_POST["password"];
+        if(loginCheck($id, $password)){
+            session_start();
+            $_SESSION['id']=$id;
+            header('Location: http://'.$_SERVER['HTTP_HOST'].'/finaltask4/view/home.php',true,303);
+            exit;
+        }
+        else{
+            echo "Error";
+        }
+    }
